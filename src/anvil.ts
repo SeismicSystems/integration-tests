7489a70f25a6cfed6d5acad0e3bb424bb0215d87
Ameya <f20200139@pilani.bits-pilani.ac.in>
2024-04-23 14:05:44 +0530

    logging for anvil functions instead of throwing errors

--- src/anvil.ts
+++ src/anvil.ts
@@ -1,9 +1,17 @@
 import { exec } from 'child_process';
 
 export function runAnvil(port: number) {
-  exec(`anvil -p ${port}`);
+  exec(`anvil -p ${port}`, (error) => {
+    if (error) {
+      console.log(`exec error: ${error}`);
+    }
+  });
 }
 
 export async function stopAnvil(port: number) {
-    exec(`kill -9 $(lsof -ti:${port})`);
+    exec(`kill -9 $(lsof -ti:${port})`, (error) => {
+      if (error) {
+        console.log(`exec error: ${error}`);
+      }
+    });
   }
