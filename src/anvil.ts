eb2eef5ddcdbbf2db67ccf48fa1be7ed788f413f
Ameya <f20200139@pilani.bits-pilani.ac.in>
2024-04-23 13:50:29 +0530

    removed logging and errors for anvil

--- src/anvil.ts
+++ src/anvil.ts
@@ -1,23 +1,9 @@
 import { exec } from 'child_process';
 
 export function runAnvil(port: number) {
-  exec(`anvil -p ${port}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  exec(`anvil -p ${port}`);
 }
 
-export function stopAnvil(port: number) {
-    exec(`kill -9 $(lsof -ti:${port})`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+export async function stopAnvil(port: number) {
+    exec(`kill -9 $(lsof -ti:${port})`);
   }
